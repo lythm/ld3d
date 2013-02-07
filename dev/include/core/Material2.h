e6da85556f33ab90639ecc466f846cd2be187881
lythm <lythm780522@gmail.com>
2013-02-07 16:52:58 +0800

    ld34

---
+++ dev/include/core/Material2.h
@@ -0,0 +1,79 @@
+#pragma once
+
+
+namespace ld3d
+{
+	class Material2
+	{
+	public:
+
+		class Parameter
+		{
+		public:
+			enum
+			{
+				pt_float,
+				pt_float2,
+				pt_float3,
+				pt_float4,
+				pt_float4x4,
+				pt_int,
+				pt_int2,
+				pt_int3,
+				pt_int4,
+				pt_bool,
+				pt_string,
+				pt_texture,
+				pt_sampler,
+
+			};
+
+			Parameter();
+			virtual ~Parameter();
+
+			const std::string&						GetName();
+			void									SetName(const std::string& name);
+
+			
+		private:
+			std::string								m_name;
+		};
+
+		template<typename T>
+		class Parameter_T : public Parameter
+		{
+		public:
+
+		};
+
+		class Pass
+		{
+		};
+
+
+		class Tech
+		{
+		public:
+
+
+		private:
+			std::vector<Pass>			m_passes;
+		};
+
+		
+
+
+
+	public:
+		Material2(void);
+		virtual ~Material2(void);
+
+		bool									LoadFromFile(const char* szFile);
+
+	private:
+		std::vector<Parameter>					m_params;
+
+		std::vector<Tech>						m_techs;
+
+	};
+}
---
+++ dev/src/core/Material2.cpp
@@ -0,0 +1,18 @@
+#include "core_pch.h"
+#include "..\..\include\core\Material2.h"
+
+namespace ld3d
+{
+	Material2::Material2(void)
+	{
+	}
+
+
+	Material2::~Material2(void)
+	{
+	}
+	bool Material2::LoadFromFile(const char* szFile)
+	{
+		return true;
+	}
+}
