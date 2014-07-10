ab21dd8a6534a0f62c3c9b4275a1c36b5643779c
Tong <lythm780522@gmail.com>
2014-07-10 14:48:17 +0800

    ld377

--- dev/src/core/RenderSystem_DR.cpp
+++ dev/src/core/RenderSystem_DR.cpp
@@ -11,5 +11,13 @@ namespace ld3d
 	RenderSystem_DR::~RenderSystem_DR(void)
 	{
 	}
-
+	bool RenderSystem_DR::Initialize(Sys_GraphicsPtr pGraphics)
+	{
+		m_pGraphics = pGraphics;
+		return true;
+	}
+	void RenderSystem_DR::Release()
+	{
+		m_pGraphics.reset();
+	}
 }
--- dev/src/core/RenderSystem_DR.h
+++ dev/src/core/RenderSystem_DR.h
@@ -4,11 +4,17 @@
 
 namespace ld3d
 {
-	class RenderSystem_DR : public RenderSystem
+	class RenderSystem_DR
 	{
 	public:
 		RenderSystem_DR(void);
 		virtual ~RenderSystem_DR(void);
+
+		bool												Initialize(Sys_GraphicsPtr pGraphics);
+		void												Release();
+
+	private:
+		Sys_GraphicsPtr							m_pGraphics;
 	};
 
 
